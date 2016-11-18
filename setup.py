525fad950f0dba766fa4f6006533d5aa5e9842cd
Sijis Aviles <sijis.aviles+github@gmail.com>
2016-11-17 23:10:36 -0600

    Ensure setup uses proper packages based on python version

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 """Tunir project"""
+import sys
 from setuptools import find_packages, setup
+
+
+requirements = [
+    'apache-libcloud',
+    'paramiko',
+    'pycrypto',
+    'systemd-python',
+    'typing',
+]
+
+if sys.version_info.major != 3:
+    requirements.append(['mock'])
 
 setup(name = 'tunir',
     version = '0.16',
@@ -11,6 +24,7 @@ setup(name = 'tunir',
     author_email="kushaldas@gmail.com",
     url="http://tunir.rtfd.org",
     license = "GPLv2+",
+    install_requires=requirements,
     packages=find_packages(),
     data_files=[('share/tunir',
                   ['examples/default.json', 'examples/multihost.txt', 'examples/multihost.cfg',
